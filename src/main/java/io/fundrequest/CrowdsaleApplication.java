c6ebd130eddae3ce65f0dc60ae159e52c17f8f20
Quinten De Swaef <quintendeswaef@gmail.com>
2016-10-07 10:51:08 +0200

    initial commit

---
+++ src/main/java/io/fundrequest/CrowdsaleApplication.java
@@ -0,0 +1,12 @@
+package io.fundrequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdsaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrowdsaleApplication.class, args);
+	}
+}
